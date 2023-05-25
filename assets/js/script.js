9d20b39cb1c004c9e967edc6713ca085b26d0a99
BrandonPCollins <bpcollins1995@gmail.com>
2023-05-25 19:48:51 +0100

    Further Introductory Box Styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 console.log("Connected")
+
+
+/* Player Name */
+
+localStorage.setItem("playerName", playerName); 
+
+function savename(){
+    var playerName = document.GetElementById("input").value;
+    localStorage.set
+}
+
+/*
+
+Math.floor(Math.random() * 10 ); 
+
+*/ 
