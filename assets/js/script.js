5212bfe09b5ca44d5a0319a5bc4ef6d4f267fb94
BrandonPCollins <bpcollins1995@gmail.com>
2023-05-26 00:16:48 +0100

    Expanding on Introduction Text

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,13 @@ function saveName(){
     console.log(playerName)
     // document.GetElementById("intro").innerHTML = "Welcome " input ; 
 
-    document.getElementById("intro").innerHTML = `<p class="fade">Hello ${playerName}.</p>` + `<p class="fade2">Hello ${playerName}.</p>`; 
+    let wrongName =  playerName.slice(1); 
+
+
+    document.getElementById("intro").innerHTML = `<p class="fade">Hello ${playerName}.</p>` + `<p class="fade2">Hmm, yes I do believe I've heard of a ${playerName}. Definitely a something-${wrongName}</p>` + `<p class="fade2">As I pondered my orb it foretold of your coming. It also told me you were a warrior of some repute. Remind me, of which school do you hail?</p>`; 
 }
 
 /*
-
 Math.floor(Math.random() * 10 ); 
 
 */ 
