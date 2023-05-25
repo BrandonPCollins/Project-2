a8776be337f3ba1cde61cb0698dbd6c11bf2a9f6
BrandonPCollins <bpcollins1995@gmail.com>
2023-05-25 22:32:37 +0100

    Background Styling & Further Text Styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@ console.log("Connected")
 
 /* Player Name */
 
+let playerName = []; 
+
 function saveName(){
-    let playerName = document.getElementById("fname").value;
+    playerName = document.getElementById("fname").value;
     console.log(playerName)
     // document.GetElementById("intro").innerHTML = "Welcome " input ; 
 
-    document.getElementById("intro").innerHTML = `<p class="fade">Hello ${playerName}.</p>`; 
-    document.getElementById("intro").style.color = red; 
+    document.getElementById("intro").innerHTML = `<p class="fade">Hello ${playerName}.</p>` + `<p class="fade2">Hello ${playerName}.</p>`; 
 }
 
 /*
