402360321c9f8950926aed8ea07c3652b108b29d
BrandonPCollins <bpcollins1995@gmail.com>
2023-05-26 01:04:40 +0100

    Class Buttons Function Implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,26 @@ function saveName(){
     let wrongName =  playerName.slice(1); 
 
 
-    document.getElementById("intro").innerHTML = `<p class="fade">Hello ${playerName}.</p>` + `<p class="fade2">Hmm, yes I do believe I've heard of a ${playerName}. Definitely a something-${wrongName}</p>` + `<p class="fade2">As I pondered my orb it foretold of your coming. It also told me you were a warrior of some repute. Remind me, of which school do you hail?</p>`; 
+    document.getElementById("intro").innerHTML = `<p class="fade">Hello ${playerName}.</p>` + `<p class="fade2">Hmm, yes I do believe I've heard of a ${playerName}. Definitely a something-${wrongName}</p>` + `<p class="fade2">As I pondered my orb it foretold of your coming. It also told me you were a warrior of some repute. Remind me, of which school do you hail?</p>` + createClasses() ; 
 }
+
+
+/* Create Classes Buttons */ 
+function createClasses(){
+    return document.getElementById("intro").innerHTML = 
+    `<div id="button-container" class="fade2">
+            <button class="button">Knight</button>
+            <button class="button">Thief</button>
+            <button class="button">Wizard</button>
+            <button class="button">Bean</button>
+    </div>`; 
+}
+
+// const gameConfig = [
+//     {
+//         story-text: "Bingy Bongo"
+//     }
+// ]
 
 /*
 Math.floor(Math.random() * 10 ); 
